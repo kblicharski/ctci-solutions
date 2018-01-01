6e4391c26b6e2e24773f66d473da4849e8296633
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-12-31 22:53:33 -0600

    Initial optimized solution to 1_5, needs cleanup.

--- Chapter 1 | Arrays and Strings/1_5_One_Away.py
+++ Chapter 1 | Arrays and Strings/1_5_One_Away.py
@@ -1,5 +1,9 @@
 """
 Problem:
+    There are three types of edits that can be performed on strings: insert
+    a character, remove a character, or replace a character. Given two
+    strings, write a function to check if they are one edit (or zero edits)
+    away.
 
 Implementation:
 
@@ -8,6 +12,11 @@ Efficiency:
     Space:
 
 """
+
+
+def ord_sum(string: str) -> int:
+    return sum([ord(c) for c in string])
+
 
 def one_away(str1: str, str2: str) -> bool:
     """
@@ -24,9 +33,28 @@ def one_away(str1: str, str2: str) -> bool:
         bool: True if string one is at most one 'edit' away from the other.
 
     """
-    pass
+    str1 = str1.lower()
+    str2 = str2.lower()
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    str1_value = ord_sum(str1)
+    str2_value = ord_sum(str2)
+    ascii_values = {ord(c) for c in alphabet}
+    difference = abs(str1_value - str2_value) 
 
+    # This handles insertion and deletion
+    # We just need a third check for replacement
+    if difference in ascii_values or difference == 0:
+        return True
 
+    s1_chars = {c for c in str1}
+    s2_chars = {c for c in str2}
+
+    count = 0
+    for c in s1_chars:
+        if c in s2_chars:
+            count += 1
+
+    return len(str1) - count <= 1
 
 # Zero edits
 assert one_away('pale', 'pale') == True
