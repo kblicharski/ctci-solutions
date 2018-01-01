e16ad5252f2f77aaf3e594b265f648bcd71ca8ab
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2018-01-01 16:41:37 -0600

    Finish my 1_6 implementation.

--- Chapter 1 | Arrays and Strings/1_6_String_Compression.py
+++ Chapter 1 | Arrays and Strings/1_6_String_Compression.py
@@ -8,11 +8,21 @@ Problem:
     letters (a-z).
 
 Implementation:
-    TODO
+    We can return early if our string is under 3 characters long, because
+    there is no way that compression would benefit it. Otherwise, we need to
+    traverse the string and count the number of consecutive characters,
+    appending them to the list that stores the "tokens" of our compressed
+    string. We then finally join the tokens back into a string and compare
+    the lengths of our compressed string and our original string to determine
+    which should be returned.
+
+    Because we have to traverse the original string in O(N) time, and then
+    join the compressed string in (worst case, when we have all unique
+    characters) O(2N) time, the total runtime is linear.
 
 Efficiency:
-    Time:
-    Space:
+    Time: O(N)
+    Space: O(N)
 
 """
 
@@ -34,4 +44,34 @@ def compressed_string(string: str) -> str:
         'abcdd'
 
     """
+    if len(string) < 3:
+        return string
+
+    compressed_chars = []
+    count = 1
+    for i, c in enumerate(string):
+        try:
+            if c == string[i + 1]:
+                count += 1
+            else:
+                compressed_chars.append(c)
+                compressed_chars.append(str(count))
+                count = 1
+        except IndexError:
+            compressed_chars.append(c)
+            compressed_chars.append(str(count))
+
+    compressed_str = ''.join(compressed_chars)
+    if len(compressed_str) < len(string):
+        return compressed_str
     return string
+
+
+assert compressed_string('') == ''
+assert compressed_string('a') == 'a'
+assert compressed_string('aa') == 'aa'
+assert compressed_string('aaa') == 'a3'
+assert compressed_string('abc') == 'abc'
+assert compressed_string('abcdefgh') == 'abcdefgh'
+assert compressed_string('aabcccccaaa') == 'a2b1c5a3'
+assert compressed_string('abcdd') == 'abcdd'
