c25e442a553c8475989bd61bab61db4a66aea7e8
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2018-01-01 19:24:07 -0600

    Improve matrix zeroing to use O(1) space.

--- Chapter 1 | Arrays and Strings/1_8_Zero_Matrix.py
+++ Chapter 1 | Arrays and Strings/1_8_Zero_Matrix.py
@@ -4,11 +4,28 @@ Problem:
     its entire row and column are set to 0.
 
 Implementation:
-    TODO
+    My initial, naive approach to the problem was to first find all
+    occurrences of zeros and add their row and column values to two lists.
+    Afterwards, we would check these lists and zero their respective rows and
+    columns. This works well, but we still need to allocate an additional
+    O(M+N) space for the two lists.
+
+    Instead, we can reduce this to O(1) space by storing this information of
+    what rows and columns to zero in the original matrix itself.
+
+    This relies on the order in which we check the values in the matrix. When
+    we check a value, we have checked all of the values preceding it -- the
+    values in all previous rows, and all previous columns of that row.
+
+    We can then set the first row at that column to zero, and the first column
+    at that row to zero. These two slices of our matrix will store the
+    information we need to zero all elements in these rows and columns. After
+    parsing the matrix for zeros, we then just need to parse the first row and
+    the first column.
 
 Efficiency:
     Time: O(MN)
-    Space: O(M+N)
+    Space: O(1)
 
 """
 from typing import List
@@ -22,25 +39,24 @@ def zero_matrix(matrix: List[List[int]]) -> None:
         matrix (List[List[int]]): The matrix we are zeroing in-place.
 
     """
-    zero_rows = []
-    zero_cols = []
-
     # Find the rows and the columns we want to zero
     for row, row_slice in enumerate(matrix):
         for col, value in enumerate(row_slice):
             if value == 0:
-                zero_rows.append(row)
-                zero_cols.append(col)
+                matrix[row][0] = 0
+                matrix[0][col] = 0
 
     # Zero the correct rows
-    for row in zero_rows:
-        for i in range(len(matrix[row])):
-            matrix[row][i] = 0
+    for row in range(1, len(matrix)):
+        if matrix[row][0] == 0:
+            for col in range(len(matrix[row])):
+                matrix[row][col] = 0
 
     # Zero the correct columns
-    for row in range(len(matrix)):
-        for col in zero_cols:
-            matrix[row][col] = 0
+    for col in range(1, len(matrix[0])):
+        if matrix[0][col] == 0:
+            for row in range(len(matrix)):
+                matrix[row][col] = 0
 
 
 # 1x1 matrix
